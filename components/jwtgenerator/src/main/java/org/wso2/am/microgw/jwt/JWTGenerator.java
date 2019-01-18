408b59a1e93b715ce0e16040fafddf8485a44bb3
tharikaGitHub <tharika.m92@gmail.com>
2019-01-18 13:58:19 +0530

    Fix license headers

--- components/jwtgenerator/src/main/java/org/wso2/am/microgw/jwt/JWTGenerator.java
+++ components/jwtgenerator/src/main/java/org/wso2/am/microgw/jwt/JWTGenerator.java
@@ -1,6 +1,5 @@
-package org.wso2.am.microgw.jwt;
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -16,6 +15,7 @@ package org.wso2.am.microgw.jwt;
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.wso2.am.microgw.jwt;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
