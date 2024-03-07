03690a086132afeb7c91a727a7e7ae300171ccf0
RakhithaRR <rakhitharr@live.com>
2024-03-07 17:30:30 +0530

    Update db scripts

--- distribution/scripts/apim/sqls/create-databases.sql
+++ distribution/scripts/apim/sqls/create-databases.sql
@@ -1,14 +1,14 @@
 drop database if exists apim;
-create database apim;
+create database apim character set latin1;
 use apim;
 source /tmp/apimgt-mysql.sql
 
 drop database if exists registry;
-create database registry;
+create database registry character set latin1;
 use registry;
 source /tmp/mysql.sql
 
 drop database if exists userstore;
-create database userstore;
+create database userstore character set latin1;
 use userstore;
 source /tmp/mysql.sql
